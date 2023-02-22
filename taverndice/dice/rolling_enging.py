f61c9f742f0c404e6bd475f315f41740cfb53c8e
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-22 08:03:04 -0600

    create rolling engine

---
+++ taverndice/dice/rolling_enging.py
@@ -0,0 +1,17 @@
+"""
+File: rolling_enging.py
+Author: Lyle Cleveland
+Email: lyle.cleveland@yahoo.com
+Github: https://github.com/lcleveland
+Description: Controls the behavior of how dice roll.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class RollingEngine(ABC):
+    """Defines the behavior of rolling engines."""
+
+    @abstractmethod
+    def roll(self) -> int:
+        """Roll a die."""
