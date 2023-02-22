d7875b02425abf6be24e70b85185114b9fb97c76
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-22 16:32:39 -0600

    add die tests

--- taverndice/tests/test_die.py
+++ taverndice/tests/test_die.py
@@ -1,8 +1,16 @@
 from hypothesis import given, strategies
+import pytest
 from ..dice.die import Die
 from ..dice.rolling_engine import PrngRollingEngine
+from ..dice.exceptions import InvalidDieSidesException
 
 
-@given(strategies.integers(min_value=1))
+@given(strategies.integers(min_value=2, max_value=1000))
 def test_set_sides(sides):
     assert Die(sides, PrngRollingEngine()).sides == sides
+
+
+@given(strategies.integers(min_value=-1, max_value=1))
+def test_sides_not_less_than_two(sides):
+    with pytest.raises(InvalidDieSidesException):
+        Die(sides, PrngRollingEngine()).sides = sides
