072adb6e52a34cfe475f174a8780b61b363700d0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-26 09:29:21 -0600

    roller

---
+++ taverndice/diceroller/roller.py
@@ -0,0 +1,13 @@
+"""
+File: roller.py
+Author: lcleveland
+Email: lyle.cleveland@yahoo.com
+Github: https://github.com/lcleveland
+Description: Converts a string of text into a series of dice rolls.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Roller:
