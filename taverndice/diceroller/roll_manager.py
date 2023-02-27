82c45ec8ebfcfe8171937ac40ccd37e49416e00a
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-27 14:26:07 -0600

    changed to roll_manager and added doc

---
+++ taverndice/diceroller/roll_manager.py
@@ -0,0 +1,7 @@
+"""
+File: roll_manager.py
+Author: Lyle Cleveland
+Email: lyle.cleveland@yahoo.com
+Github: https://github.com/lcleveland
+Description: Manages rolling a group of dice and returning the results.
+"""
