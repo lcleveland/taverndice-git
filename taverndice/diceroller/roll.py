ae967f02cc17ff6d55d095cec6fe0443505a0667
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-27 14:41:49 -0600

    add a manager and a managed class

---
+++ taverndice/diceroller/roll.py
@@ -0,0 +1,7 @@
+"""
+File: roll.py
+Author: lcleveland
+Email: lyle.cleveland@yahoo.com
+Github: https://github.com/lcleveland
+Description: Converts a string of text into a series of dice rolls.
+"""
