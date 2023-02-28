520e84307a20fff65ee97e9d3497ed023371e43d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-02-28 10:12:30 -0600

    change description

--- taverndice/diceroller/roll.py
+++ taverndice/diceroller/roll.py
@@ -3,5 +3,5 @@ File: roll.py
 Author: lcleveland
 Email: lyle.cleveland@yahoo.com
 Github: https://github.com/lcleveland
-Description: Converts a string of text into a series of dice rolls.
+Description: Handles rolling dice.
 """
